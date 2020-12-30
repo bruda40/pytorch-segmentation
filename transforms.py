15bb130afeb1fb9965cf262a8d1b5f18fea76a4b
Juozas Polikevičius <onixaz@gmail.com>
2020-12-30 08:05:00 +0200

    Revert "Dev"

--- transforms.py
+++ transforms.py
@@ -36,7 +36,6 @@ class Resize(object):
         target = F.resize(target, self.size, interpolation=Image.NEAREST)
         return image, target
 
-
 class RandomResize(object):
     def __init__(self, min_size, max_size=None):
         self.min_size = min_size
